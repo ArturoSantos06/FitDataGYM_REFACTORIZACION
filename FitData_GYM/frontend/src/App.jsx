200901466a1b04f77e17d0802551d0dc0bc61fd9
ArturoSantos06 <artsantoss30@gmail.com>
2026-07-20 13:41:45 -0600

    Refactorizacion Inventario

--- FitData_GYM/frontend/src/App.jsx
+++ FitData_GYM/frontend/src/App.jsx
@@ -10,7 +10,7 @@ import AssignMembership from './components/administrador/AsignarMembresia';
 import UserMembershipList from './components/administrador/ListaMembresiasUsuario';
 import MembershipAdmin from './components/administrador/AdministrarMembresias';
 import PuntoDeVenta from './components/administrador/Venta/PuntoDeVenta';
-import Inventario from './components/administrador/Inventario';
+import Inventario from './components/administrador/Inventario/Inventario';
 import CheckInOut from './components/administrador/CheckInOut';
 import HealthProfilesAdmin from './components/administrador/PerfilesSaludAdmin';
 import BitacoraEntrenador from './components/entrenador/seguimiento/BitacoraEntrenador';
